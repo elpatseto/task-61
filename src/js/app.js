857a9ca9d770bcdc3122042b91bc5f01208cfea4
Plamen Georgiev <p.olegov@yahoo.com>
2023-01-16 10:54:17 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,8 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
+
+ let hotPrice = document.querySelectorAll(".hot");
+    for (let i = 0; i < hotPrice.length ; i++) {
+        hotPrice[i].innerHTML += '&#128293;';
+    }
